35ca18c0477cf0532ee21be86b36acaf7c0def7a
Dangal-Santosh <santoshdangal79@gmail.com>
2023-05-22 21:16:54 +0545

    Modified main

--- main.py
+++ main.py
@@ -1,88 +1,63 @@
+from FlightRadar24.api import FlightRadar24API
+import re
 import csv
-import random
-from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+import pandas as pd
+from itertools import count
+from matplotlib.animation import FuncAnimation
 
-# Generate random flight data
-def generate_flight_data():
-    flight_name = ''.join(random.choices('ABCDEFGHIJKLMNOPQRSTUVWXYZ', k=6))
-    departure_airport = ''.join(random.choices('ABCDEFGHIJKLMNOPQRSTUVWXYZ', k=3))
-    arrival_airport = ''.join(random.choices('ABCDEFGHIJKLMNOPQRSTUVWXYZ', k=3))
-    airline = ''.join(random.choices('ABCDEFGHIJKLMNOPQRSTUVWXYZ', k=2))
-    return [flight_name, departure_airport, arrival_airport, airline]
+fr_api = FlightRadar24API()
+flights = fr_api.get_flights()
 
-# Define the CSV file path
-csv_file = 'flight_data_flightradar24.csv'
-
-# Generate flight data for 1000 flights
 data = []
-for _ in range(1000):
-    data.append(generate_flight_data())
+for flight in flights:
+    data.append(str(flight))
 
-# Write data to CSV file
-with open(csv_file, 'w', newline='') as file:
+l = []
+a = []
+c = []
+for d in data:
+    main_data = d.strip("<>").strip()
+    split_data = main_data.split(" - ")
+    l.append(split_data[0])
+    a.append(split_data[1])
+    for g in a:
+        c.append(g[9:])
+
+csv_file = 'flight_data.csv'
+
+with open(csv_file, mode='w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(['Flight Name', 'Departure Airport', 'Arrival Airport', 'Airline'])  # Write header
-    writer.writerows(data)  # Write data rows
+    writer.writerow(['Flight', 'Altitude'])
 
-print(f"Random flight data has been saved to {csv_file}.")
+    for i in range(len(l)):
+        writer.writerow([l[i], c[i]])
 
-# Set up the figure and axis
-fig, ax = plt.subplots()
+# For visualization
+plt.style.use('seaborn-darkgrid')
 
-# Initialize the empty plot
-flight_names = []
-departure_airports = []
-arrival_airports = []
-airlines = []
-flight_plot = ax.bar([], [], align='edge', color='deepskyblue', edgecolor='black', linewidth=0.5)
+df = pd.read_csv('flight_data.csv')
 
-# Set the axis labels and title
-ax.set_xlabel('Flight Name', fontsize=12)
-ax.set_ylabel('Count', fontsize=12)
-ax.set_title('Real-time Flight Data Visualization', fontsize=14, fontweight='bold')
+fig, ax = plt.subplots(figsize=(8, 6))
 
-# Set the plot style
-plt.style.use('seaborn-whitegrid')
+def animate(i):
+    plt.cla()
 
-# Function to update the plot
-def update_plot(frame):
-    # Clear the previous plot
-    ax.clear()
+    start_index = i * 20
+    end_index = (i + 1) * 20
 
-    # Read the updated flight data from CSV file
-    with open(csv_file, 'r') as file:
-        reader = csv.reader(file)
-        next(reader)  # Skip the header row
-        flight_data = list(reader)
+    x_vals = df['Flight'].iloc[start_index:end_index]
+    y_vals = df['Altitude'].iloc[start_index:end_index]
 
-    # Extract flight data columns
-    flight_names = [row[0] for row in flight_data[frame:frame+20]]
-    departure_airports = [row[1] for row in flight_data[frame:frame+20]]
-    arrival_airports = [row[2] for row in flight_data[frame:frame+20]]
-    airlines = [row[3] for row in flight_data[frame:frame+20]]
+    ax.plot(x_vals, y_vals, marker='o', linestyle='-', color='b')
 
-    # Update the plot with new data
-    flight_plot = ax.bar(flight_names, range(len(flight_names)), align='edge',
-                         color='deepskyblue', edgecolor='black', linewidth=0.5)
+    # Customize plot appearance
+    ax.set_xlabel('Flight')
+    ax.set_ylabel('Altitude')
+    ax.set_title('Flight Altitude Visualization')
+    ax.tick_params(axis='x', rotation=45)
+    ax.set_ylim(bottom=0)  # Set minimum y-axis value to 0
 
-    # Set the axis labels and title
-    ax.set_xlabel('Flight Name', fontsize=12, fontweight='bold')
-    ax.set_ylabel('Count', fontsize=12, fontweight='bold')
-    ax.set_title('Real-time Flight Data Visualization', fontsize=14, fontweight='bold')
-
-    # Rotate the x-axis labels for better visibility
-    plt.xticks(rotation=45, ha='right', fontsize=8)
-
-    # Set the plot background color
-    ax.set_facecolor('#f0f0f0')
-
-# Calculate the total number of frames
-total_frames = len(data) - 20
-
-# Animate the plot
-ani = animation.FuncAnimation(fig, update_plot, frames=total_frames, interval=4000, repeat=False)
-
-# Display the plot
+ani = FuncAnimation(fig, animate, frames=len(df) // 20, interval=8000)
+plt.tight_layout()
 plt.show()
